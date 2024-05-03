0d8b81a634d6ff29a7b79f332ad3e0c5d8969d48
abdelkrim ouaaddi <abdelkrimouaaddi@gmail.com>
2024-05-03 20:11:45 +0900

    update vite

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       {
-        path: "/",
+        path: "/kasbah-cuisine/",
         element: <LandingPage />,
       },
       {
