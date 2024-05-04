dd40e4cb7f07c1f5e66b0368366ce177d51f4f24
abdelkrim ouaaddi <abdelkrimouaaddi@gmail.com>
2024-05-04 10:09:46 +0900

    update few files

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/kasbah-cuisine/",
+  base: "/",
   plugins: [react(), eslint()],
   build: {
     outDir: "dist", // Ensuring that the output directory is explicitly set
