b83dac32ca9fb726d796ad7ecb2d9fb58b29d3ea
abdelkrim ouaaddi <abdelkrimouaaddi@gmail.com>
2024-05-04 09:08:49 +0900

    not giving up

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/kasbah-cuisine/",
+  base: "/kasbah-cuisine",
   plugins: [react(), eslint()],
 });
